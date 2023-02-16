afb0a2a042a0cbba99c4606efa7f366f680f720c
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-02-16 07:32:06 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Aprogram that prints a line using printf not puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
