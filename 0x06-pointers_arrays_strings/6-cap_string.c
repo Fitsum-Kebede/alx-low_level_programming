b0d8609f92bc943a18452da32bde33bf4ceebd42
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-03-02 06:49:33 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - a function that capitalizes all words of a string
- * @n: input string
- * Return: caps on first letter of a separator
+ * cap_string - Capiralizes all words of a string
+ * @str: the string to be captalized
+ * Return: A pointer to the changed string.
  */
-char *cap_string(char *n)
+char *cap_string(char *str)
 {
-	int i, x;
-	int cap = 32;
-	int separators[] = {',', ';', '.', '?', '"',
-	'(', ')', '{', '}', ' ', '\n', '\t'};
+	int index = 0;
 
-	for (i = 0; n[i] != '\0'; i++)
+	while (str[index])
 	{
-	if (n[i] >= 'a' && n[i] <= 'z')
-	{
-	n[i] = n[i] - cap;
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
+	index++;
+	if (str[index - 1] == ' ' ||
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
+	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
+	str[index - 1] == '(' ||
+	str[index - 1] == ')' ||
+	str[index - 1] == '{' ||
+	str[index - 1] == '}' ||
+	index == 0)
+	str[index] -= 32;
+	index++;
 	}
-	cap = 0;
-	for (x = 0; x <= 12; x++)
-	{
-	if (n[i] == separators[x])
-	{
-	x = 12;
-	cap = 32;
-	}													}
-	}
-	}
-	return (n);
+return (str);
 }
