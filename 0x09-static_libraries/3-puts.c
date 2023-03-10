a874fa7c020227063ab11120cbfec7e47cafb9cd
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-03-10 04:46:41 -0800

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _puts - a function that prints a string
+* @str: string input
+* Return: string
+*/
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
