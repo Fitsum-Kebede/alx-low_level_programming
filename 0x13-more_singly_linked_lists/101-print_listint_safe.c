72e99a09a0b54461c04b91ae7a6b06d3cf6773c3
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-04-04 12:27:57 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,9 +3,9 @@
 /**
  * free_listp - frees a linked list
  * @head: head of a list.
- * Return: 0
+ *
+ * Return: no return.
  */
-
 void free_listp(listp_t **head)
 {
 	listp_t *temp;
@@ -13,22 +13,22 @@ void free_listp(listp_t **head)
 
 	if (head != NULL)
 	{
-	curr = *head;
-	while ((temp = curr) != NULL)
-	{
-	curr = curr->next;
-	free(temp);
-	}
-	*head = NULL;
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
 }
 
 /**
  * print_listint_safe - prints a linked list.
  * @head: head of a list.
+ *
  * Return: number of nodes in the list.
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nnodes = 0;
@@ -37,31 +37,31 @@ size_t print_listint_safe(const listint_t *head)
 	hptr = NULL;
 	while (head != NULL)
 	{
-	new = malloc(sizeof(listp_t));
+		new = malloc(sizeof(listp_t));
 
-	if (new == NULL)
-	exit(98);
+		if (new == NULL)
+			exit(98);
 
-	new->p = (void *)head;
-	new->next = hptr;
-	hptr = new;
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
 
-	add = hptr;
+		add = hptr;
 
-	while (add->next != NULL)
-	{
-	add = add->next;
-	if (head == add->p)
-	{
-	printf("-> [%p] %d\n", (void *)head, head->n);
-	free_listp(&hptr);
-	return (nnodes);
-	}
-	}
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
+		}
 
-	printf("[%p] %d\n", (void *)head, head->n);
-	head = head->next;
-	nnodes++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
 	}
 
 	free_listp(&hptr);
