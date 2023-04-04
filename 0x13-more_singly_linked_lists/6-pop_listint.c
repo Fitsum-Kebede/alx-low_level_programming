f22aeb1318303d0e6ef4d41035b0f9469f5a03bb
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-04-04 11:47:58 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,15 +7,15 @@
  */
 int pop_listint(listint_t **head)
 {
-	int first_node;
+	int node;
 	listint_t *temp, *next;
 
 	if (*head == NULL)
 	return (0);
 	temp = *head;
 	next = temp->next;
-	first_node = temp->n;
+	node = temp->n;
 	free(temp);
 	*head = next;
-	return (first_node);
+	return (node);
 }
