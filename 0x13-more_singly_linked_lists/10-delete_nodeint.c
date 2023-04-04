491ba26d0c594bb41d5304a93ea733f520665816
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-04-04 09:43:18 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,23 +9,25 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
 	listint_t *prev;
 	listint_t *next;
-	unsigned int nu;
 
 	prev = *head;
+
 	if (index != 0)
 	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
 
-	for (nu = 0; nu < index - 1 && temp != NULL; nu++)
-	{
-		prev = prev->next;
-	}
-	}
 	if (prev == NULL || (prev->next == NULL && index != 0))
 	{
 		return (-1);
 	}
+
 	next = prev->next;
 
 	if (index != 0)
@@ -35,8 +37,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-	free(prev);
-	*head = next;
+		free(prev);
+		*head = next;
 	}
+
 	return (1);
-	}
+}
