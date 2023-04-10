66d46299478153c7d819ffcdaee0d672c92e7f47
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-04-10 13:17:57 -0700

    0-read_textfile.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -56,43 +56,44 @@ int main(int argc, char *argv[])
 {
 	int f;
 	int t;
-
+	int rr;
+	int o;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 	}
 
 	buffer = create_buffer(argv[2]);
 	f = open(argv[1], O_RDONLY);
-	re = read(f, buffer, 1024);
+	rr = read(f, buffer, 1024);
 	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-	if (from == -1 || r == -1)
+	if (f == -1 || rr == -1)
 	{
 	dprintf(STDERR_FILENO,
 	"Error: Can't read from file %s\n", argv[1]);
 	free(buffer);
 	exit(98);
 	}
-	e = write(t, buffer, re);
-	if (to == -1 || w == -1)
+	o = write(t, buffer, rr);
+	if (t == -1 || o == -1)
 	{
 	dprintf(STDERR_FILENO,
 	"Error: Can't write to %s\n", argv[2]);
 	free(buffer);
 	exit(99);
 	}
-	re = read(from, buffer, 1024);
+	rr = read(from, buffer, 1024);
 	t = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (re > 0);
 
 	free(buffer);
-	close_file(from);
+	close_file(f);
 	close_file(t);
 
 	return (0);
