6249e3b7ac4e738dff0804e31cead59dd38943e2
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-04-10 12:41:45 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file- apends text to file
+ * @filename: path to a file
+ * @text_content: content
+ * Return: Returns 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int t;
+	int f;
+
+	if (filename == NULL)
+	return (-1);
+	f = open(filename, O_WRONLY | O_APPEND);
+	if (f == -1)
+	return (-1);
+	if (text_content != NULL)
+	{
+	t = write(fd, text_content, strlen(text_content));
+	if (t == -1)
+	return (-1);
+	}
+	close(f);
+	return (1);
+}
