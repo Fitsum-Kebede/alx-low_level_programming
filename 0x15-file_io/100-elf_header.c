6a5d1858ff0480a97c3e2cb2aea96a43ee4353a2
Fitsum Kebede <123200223+Fitsum-Kebede@users.noreply.github.com>
2023-04-11 00:11:18 +0300

    Update 100-elf_header.c
    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -278,7 +278,7 @@ void close_elf(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int n, r;
+	int n, ee;
 
 	n = open(argv[1], O_RDONLY);
 	if (n == -1)
@@ -293,8 +293,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(n, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	ee = read(n, header, sizeof(Elf64_Ehdr));
+	if (ee == -1)
 	{
 		free(header);
 		close_elf(n);
