09b8e5e04addcc97fcc127efbb14868f9e4692c9
Fitsum Kebede <123200223+Fitsum-Kebede@users.noreply.github.com>
2023-04-11 15:40:52 +0300

    Update 3-cp.c
    Another update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,15 +16,16 @@ void _error(int e, char *filename)
 {
 	if (e == 98)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
 	}
 	if (e == 99)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
 	}
 }
+
 /**
  * cp - Copies the content of a file to another file.
  * @file_from: Name of file
@@ -40,37 +41,33 @@ void cp(char *file_from, char *file_to)
 	int fd_w;
 	char *buf[1024];
 
-	/* READ */
 	fd = open(file_from, O_RDONLY);
 	if (fd < 0)
-		_error(98, file_from);
-	/* WRITE */
+	_error(98, file_from);
 	fd_w = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_w < 0)
 	{
-		close(fd);
-		_error(99, file_to);
+	close(fd);
+	_error(99, file_to);
 	}
 	do {
-		/* READ */
-		res_read = read(fd, buf, 1024);
-		if (res_read < 0)
-			_error(98, file_from);
-		/* WRITE */
-		res_write = write(fd_w, buf, res_read);
-		if (res_write < res_read)
-			_error(99, file_to);
+	res_read = read(fd, buf, 1024);
+	if (res_read < 0)
+	_error(98, file_from);
+	res_write = write(fd_w, buf, res_read);
+	if (res_write < res_read)
+	_error(99, file_to);
 	}	while (res_write == 1024);
 	if (close(fd) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		close(fd_w);
-		exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	close(fd_w);
+	exit(100);
 	}
 	if (close(fd_w) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
-		exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+	exit(100);
 	}
 }
 /**
@@ -84,8 +81,8 @@ int main(int ac, char *av[])
 {
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 	}
 	cp(av[1], av[2]);
 	return (0);
