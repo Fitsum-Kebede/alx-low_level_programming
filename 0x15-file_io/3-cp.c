7d777349e8d89ffca337907e3e1dfdec6c4093bb
Fitsum Kebede <123200223+Fitsum-Kebede@users.noreply.github.com>
2023-04-11 15:37:27 +0300

    Update 3-cp.c
    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,11 +5,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
- * _error - Read file.
+ * _error - checks if files can be opened
  * @e: Error number
  * @filename: File name
  */
+
 void _error(int e, char *filename)
 {
 	if (e == 98)
@@ -25,54 +27,59 @@ void _error(int e, char *filename)
 }
 /**
  * cp - Copies the content of a file to another file.
- * @file_from: Name of the source file.
- * @file_to: Name of the destination file.
- * Return: 1 on success, -1 on failure.
+ * @file_from: Name of file
+ * @file_to: Name of the destination file
+ * Return: 1 or -1
  */
+
 void cp(char *file_from, char *file_to)
 {
-	int fd_read, res_read, fd_write, res_write;
+	int res_read;
+	int fd;
+	int res_write;
+	int fd_w;
 	char *buf[1024];
 
 	/* READ */
-	fd_read = open(file_from, O_RDONLY);
-	if (fd_read < 0)
+	fd = open(file_from, O_RDONLY);
+	if (fd < 0)
 		_error(98, file_from);
 	/* WRITE */
-	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_write < 0)
+	fd_w = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_w < 0)
 	{
-		close(fd_read);
+		close(fd);
 		_error(99, file_to);
 	}
 	do {
 		/* READ */
-		res_read = read(fd_read, buf, 1024);
+		res_read = read(fd, buf, 1024);
 		if (res_read < 0)
 			_error(98, file_from);
 		/* WRITE */
-		res_write = write(fd_write, buf, res_read);
+		res_write = write(fd_w, buf, res_read);
 		if (res_write < res_read)
 			_error(99, file_to);
 	}	while (res_write == 1024);
-	if (close(fd_read) < 0)
+	if (close(fd) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
-		close(fd_write);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		close(fd_w);
 		exit(100);
 	}
-	if (close(fd_write) < 0)
+	if (close(fd_w) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 }
 /**
- * main - Copies the content of a file to another file.
+ * main - Copies the content of a file
  * @ac: Argument count
  * @av: argument values
- * Return: 0 on succes, -1 on error.
+ * Return: 0 or -1
  */
+
 int main(int ac, char *av[])
 {
 	if (ac != 3)
