142f7b1ff48c1040a8ea5cd62fa57a59b45a6552
fitsum-kebede <fitsuminfome@gmail.com>
2023-10-11 06:28:06 -0700

    0x1E-search_algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,21 +1,31 @@
 #include "search_algos.h"
+
 /**
-* linear_search -  linear search algorithm
-* @array: is a pointer to the array to search
-* @size: is the number of elements in array
-* @value: is the value to search
-* Return: 0 or -1
-*/
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i = 0;
+	size_t x;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL)
 	{
-		printf("Value checked array[%i] = [%i]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		return (-1);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
 	}
 	return (-1);
 }
